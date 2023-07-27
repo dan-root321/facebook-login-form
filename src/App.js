4253bb584b3b80ddc17bc52e76fe02f821dafce2
#Kali <brucaldan@gmail.com>
2023-07-27 18:35:52 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Banner() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="container">
+        <div className="banner">
+          <div className="left-div">
+            <div className="fb_logo"></div>
+            <h2 className="recent-login">Recent Login</h2>
+            <p className="text">Click your picture or add an account.</p>\
+            <div class="card">
+              <div className="photocard"></div>
+              <div class="card__content">
+                <p class="card__title">Card Title</p>
+                <p class="card__description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="right-div">
+            <form class="form">
+              <p class="form-title">Sign in to your account</p>
+              <div class="input-container">
+                <input type="email" placeholder="Enter email"></input>
+                <span></span>
+              </div>
+              <div class="input-container">
+                <input type="password" placeholder="Enter password"></input>
+              </div>
+              <button type="submit" class="submit">
+                Sign in
+              </button>
+
+              <p class="signup-link">
+                No account?
+                <a href="">Sign up</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function MainPage() {
+  return (
+    <div className="login_page">
+      <Banner />
     </div>
   );
 }
 
-export default App;
+export default MainPage;
